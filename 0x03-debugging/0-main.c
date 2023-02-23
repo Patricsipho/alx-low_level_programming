3be734f75ac7eae886204ec63824ada94404281d
Patricsipho <Siphommadirafx@gmail.com>
2023-02-23 11:25:30 -0800

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,11 +4,13 @@
  * main - tests function that prints if position or nagative
  * Return: 0
  */
+
 int main(void)
 {
-       int i;
-       i = 0;
-       positive_or_negative(i);
+ int i;
+ i = 0;
 
-       return (0);     
+positive_or_negative(i); 
+
+return (0);     
 }
