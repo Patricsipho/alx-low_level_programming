0cdd5317bc41d56646cdd26cf9b9ded1e2747e4c
Patricsipho <Siphommadirafx@gmail.com>
2023-02-27 07:07:24 -0800

    1-alphabet

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - writes the charecter c to  stdout 
+ * @c: The charecter to print 
+ *
+ */
+
+int _putchar(char c)
+{
+	return (write(0, &c, 1));
+}	
+
