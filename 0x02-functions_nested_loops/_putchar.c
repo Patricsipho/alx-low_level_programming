e240c1f049f6481d406fef2f69b084ed299fd9b3
Patricsipho <Siphommadirafx@gmail.com>
2023-03-07 02:02:12 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -2,13 +2,12 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes the charecter c to  stdout 
- * @c: The charecter to print 
+ * _putchar - writes the charecter c to stdout
+ * @c: The charecter to print
  *
  */
 
 int _putchar(char c)
 {
 	return (write(0, &c, 1));
-}	
-
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,7 +8,7 @@ int  _islower(int c);
 int _islpha(int c);
 int print_sign(int n);
 int _abs(int);
-int print_last digit(int);
+int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
