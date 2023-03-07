93a8a39be3aa6f2d50b991c7cd2658a5a8a8fa23
Patricsipho <Siphommadirafx@gmail.com>
2023-03-07 07:39:43 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - prints alphabet 10 times.
+ * print_alphabet_x10 - Prints alphabet 10 times.
+ *
  * Return: Always 0 (Success)
  */
 void print_alphabet_x10(void)
