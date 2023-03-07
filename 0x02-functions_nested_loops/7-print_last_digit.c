b32aea15ba2fbbdedffaf2d64050f51f5246cc89
Patricsipho <Siphommadirafx@gmail.com>
2023-03-07 09:11:44 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,11 +7,18 @@
  */
 int print_last_digit(int n)
 {
-	int ld = n % 10;
+	int l;
 
-	if (ld <= 0)
-		ld <= -1;
-	_putchar(ld + '0');
+	l = n % 10;
+	if (l < 0)
+	{
+		_putchar(-l + 48);
+		return (-l);
+	}
+	else
+	{
+		_putchar(l + 48);
 
-	return (0);
+		return (l);
+	}
 }
